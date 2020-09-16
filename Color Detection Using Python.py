ee5e5264c3a85bb7a4fd231f5ddc67ecc86ebd4e
Pradeep-Shinde <71371117+Pradeep-Shinde@users.noreply.github.com>
2020-09-16 13:26:49 +0530

    Update Color Detection Using Python.py

--- Color Detection Using Python.py
+++ Color Detection Using Python.py
@@ -1,10 +1,14 @@
+# -------------------------------------------------------------------------
+# --- Author         : Pradeep Shinde
+# --- Date           : 16th September 2020
+# -------------------------------------------------------------------------
+
 # import the necessary packages
 import numpy as np
 import cv2
 import pandas as pd
 
-# initialize the list of reference points and boolean indicating
-# whether cropping is being performed or not
+# initialize the list of reference points and boolean indicating whether cropping is being performed or not
 refPt = []
 cropping = False
 
@@ -20,34 +24,20 @@ roi=image
 def click_and_crop(event, x, y, flags, param):
 	# Grab references to the global variables
 	global refPt, cropping
-	# if the left mouse button was clicked, record the starting
-	# (x, y) coordinates and indicate that cropping is being
-	# performed
+	# if the left mouse button was clicked, record the starting (x, y) coordinates and indicate that cropping is being performed
 	if event == cv2.EVENT_LBUTTONDOWN:
 		refPt = [(x, y)]
 		cropping = True
 	# check to see if the left mouse button was released
 	elif event == cv2.EVENT_LBUTTONUP:
-		# record the ending (x, y) coordinates and indicate that
-		# the cropping operation is finished
+		# record the ending (x, y) coordinates and indicate that the cropping operation is finished
 		refPt.append((x, y))
 		cropping = False
 		# draw a rectangle around the region of interest
 		cv2.rectangle(image, refPt[0], refPt[1], (0, 255, 0), 2)
 		cv2.imshow("image", image)
 
-# Function to calculate minimum distance from all colors and get the most matching color
-def getColorName(R, G, B):
-	minimum = 10000
-	for i in range(len(csv)):
-		d = abs(R - int(csv.loc[i, "R"])) + abs(G - int(csv.loc[i, "G"])) + abs(B - int(csv.loc[i, "B"]))
-		if (d <= minimum):
-			minimum = d
-			cname = csv.loc[i, "color_name"]
-	# print('color name : ',cname)
-	return cname
-
-#Function to return R,G,B values
+#Function to return R,G,B values using k-means clustering Algorithm
 def Color_Recognition_Kmeans(image_cropped):
 	print('Color Recognition.....')
 
@@ -87,6 +77,16 @@ def Color_Recognition_Kmeans(image_cropped):
 	rgb = center[0]
 	return rgb
 
+# Function to calculate minimum distance from all colors and get the most matching color
+def getColorName(R, G, B):
+	minimum = 10000
+	for i in range(len(csv)):
+		d = abs(R - int(csv.loc[i, "R"])) + abs(G - int(csv.loc[i, "G"])) + abs(B - int(csv.loc[i, "B"]))
+		if (d <= minimum):
+			minimum = d
+			cname = csv.loc[i, "color_name"]
+	return cname
+
 clone = image.copy()
 cv2.namedWindow("image")
 cv2.setMouseCallback("image", click_and_crop)
@@ -102,7 +102,6 @@ while True:
 	if key == ord("c"):
 		break
 	# if there are two reference points, then crop the region of interest
-	# from teh image and display it
 	if len(refPt) == 2:
 		roi = clone[refPt[0][1]:refPt[1][1], refPt[0][0]:refPt[1][0]]
 	rgb=Color_Recognition_Kmeans(roi)
